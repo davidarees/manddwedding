33fd6ffcaeaff3ece2fca9bf6428abc6a835a469
David Rees <davidarees@outlook.com>
2014-12-31 16:49:21 +0000

    changed route to be new session, created a couple of seeds, changed to use username not email

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+u1 = User.create(username: 'guest', email: 'guest@marthaanddave.info', password: 'barnet', password_confirmation: 'barnet')
+
+u2 = User.create(username: 'fennell', email: 'fennell@marthaanddave.info', password: 'foresthill', password_confirmation: 'foresthill') 
