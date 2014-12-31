8358008c8354d06a0e062e53546ceec74d385b6f
David Rees <davidarees@outlook.com>
2014-12-31 12:45:31 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MandDWedding::Application.config.secret_token = 'dd88c821c71d16dd367ca03e573cbb5030ee2978a32c9e6c9d3bd0757781f34a2e78fa68144afd679710510e51603aaa3bef723d4bbc9bf7bc560c3c9254085b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MandDWedding::Application.config.session_store :cookie_store, key: '_MandDWedding_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MandDWedding::Application.config.session_store :active_record_store
