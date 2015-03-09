dd39b435b4c0424539a90cd4838459750d44df24
David Rees <davidarees@outlook.com>
2015-03-09 22:35:31 +0000

    menubar fixed and  colours of bar fixed

---
+++ app/assets/javascripts/menu.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+  $(".nav li a").on("click", function(){
+     $(".nav").find(".active").removeClass("active");
+     $(this).parent().addClass("active");
+  });
+
+});
