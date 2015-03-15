5a14309a6d6281e6e656d4e0d5c1dfbbd4d3c7ae
David Rees <davidarees@outlook.com>
2015-03-15 11:55:00 +0000

    fixed password length

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-u1 = User.create(username: 'guest', email: 'guest@marthaanddave.info', password: 'barnet', password_confirmation: 'barnet')
+u1 = User.create!(username: 'guest', email: 'guest@marthaanddave.info', password: 'BARNET', password_confirmation: 'BARNET')
 
-u2 = User.create(username: 'fennell', email: 'fennell@marthaanddave.info', password: 'foresthill', password_confirmation: 'foresthill') 
+u2 = User.create!(username: 'fennell', email: 'fennell@marthaanddave.info', password: 'foresthill', password_confirmation: 'foresthill')  
